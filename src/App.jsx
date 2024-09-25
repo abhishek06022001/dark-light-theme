3b9a9455f666dc010057a8a6d9a1fe603f301b55
Abhishek <satheabhishek1@gmail.com>
2024-09-25 09:23:20 +0530

    left little understanding the css of toggle

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+import './App.css'
+import Toggle from './components/Toggle';
+function App() {
+  const [isDark, setIsDark] = useState(false);
+  return (
+    <>
+      <div className='App' data-theme={isDark ? 'dark' : 'light'}>    
+          <Toggle
+            isChecked={isDark}
+            handleChange={() => setIsDark(!isDark)}
+          />    
+        <h1 className="title">Hello World</h1>
+        <div className="box">
+          <h2>This is a box </h2>
+        </div>
+      </div>
+    </>
+  )
+}
+export default App
---
+++ src/components/Toggle.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./Toggle.css"
+function Toggle({ handleChange, isChecked }) {
+    return (
+        <div className="toggle-container">
+            <input type="checkbox"
+                id='check'
+                className='toggle'
+                onChange={handleChange}
+                checked={isChecked}
+
+            />
+            <label htmlFor="check">Dark Mode</label>
+        </div>
+    )
+}
+
+export default Toggle
